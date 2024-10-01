6af689d9bc9aef629d73c8ad0f229f94c37c2058
markbasiliano <mbasiliano28@gmail.com>
2024-10-01 11:05:46 -0600

    added a bunch of stuff that i don't understand

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello from React in your existing project!</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app'; // Main React component
+
+// Render your React app to the root element
+ReactDOM.render(<App />, document.getElementById('root'));
